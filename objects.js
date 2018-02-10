d686b92795e605e6347d098dbfdb68d1aa821a74
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-10 22:31:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,4 @@ var meals = { breakfast: "oatmeal" } ;
 // breakfast is the key and oatmeal is the value
 
 var meals = {[firstMeal] : "oatmeal"}
+Object.assign ( {}, meals, {breakfast: [ 'oatmeal', 'banana']})
