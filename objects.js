dd15b8fb8941defd0ba99bf5c1f958909129fbb9
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-10 22:37:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-var playlist = 
+var playlist = { artist: 'Justin Bieber' , song: 'baby'}
+
+function updatePlaylist(object, Justin, baby) {
+  return playlist.artist
+}
+
+function removeFromPlaylist(object, Justin) {
+  delete playlist.artist
+}
